a4629422768e08f500a4d0f3452e93f8f0cf304d
takashi002013@gmail.com <takashi002013@gmail.com>
2021-10-03 18:34:08 +0900

    first-commit

--- engine/amazon_scraping.py
+++ engine/amazon_scraping.py
@@ -23,79 +23,9 @@ class AmazonScraping():
         self.driver = set_driver()
 
 
-    def fetch_item(self, url: str) -> AmazonItem:
-        self.driver.get(url)
-        
-        # 商品名
-        try:
-            name = self.driver.find_element_by_css_selector("#productTitle").text.strip()
-        except:
-            name = None
-            
-        # 説明
-        try:
-            description = self.driver.find_element_by_css_selector("#productDescription").text.strip().replace("\n","")
-        except:
-            description = None
-
-        # 価格
-        try:
-            price = int(self.driver.find_element_by_css_selector("#priceblock_ourprice").text.replace("￥","").replace(",",""))
-        except:
-            price = None
-        
-        # 星の数
-        try:
-            star = float(self.driver.find_element_by_css_selector(".a-icon-alt").text.replace("5つ星のうち",""))
-        except:
-            star = None
-        
-        # ASIN (urlの/dp/の後の英数字10桁)
-        m = re.search(r"/dp/(\w{10})", url)
-        try:
-            asin = m.group(1)
-        except:
-            asin = None
-        
-        return AmazonItem(
-            name=name, description=description, price=price,
-            asin=asin, star=star
-        )
+    def fetch_item(self, url: str):
+        pass
 
     
-    def fetch_ranking_items(self, url: str, limit:int=50) -> list:
-        # Driver起動
-        try:
-            driver = set_driver()
-        except:
-            print("selenium driver起動エラー")
-            raise Exception("selenium driver起動エラー")
-        
-        # ランキングページを開く
-        driver.get(url)
-        
-        # ランキングitemの要素を取得
-        item_elms = driver.find_elements_by_css_selector(".aok-inline-block.zg-item > a")
-                
-        # 商品詳細のリンクを取得
-        item_links = []
-        for i, item_elm in enumerate(item_elms[:limit]):
-            try:
-                # linkを取得
-                item_links.append(item_elm.get_attribute("href"))
-            except Exception as e:
-                pass
-        print(f"item count: {len(item_links)}")
-        
-        items = []    
-        for i, item_link in enumerate(item_links):
-            try:
-                # itemの情報を取得
-                item = self.fetch_item(item_link)
-                items.append(item)
-                print(f"{i+1} 個目取得完了")
-            except Exception as e:
-                print(f"error {item_link} | {e}")
-        print(len(items))
-
-        return items
+    def fetch_ranking_items(self, url: str, limit:int=50):
+        pass
--- main/run.py
+++ main/run.py
@@ -8,14 +8,8 @@ from engine.amazon_scraping import *
 
 
 def run(url: str, limit:int=50):
-    amazon = AmazonScraping()
-    items = amazon.fetch_ranking_items(url, limit)
-    df = pd.DataFrame()
-    for item in items:
-        df = df.append(item.__dict__, ignore_index=True).fillna("")
-    df.to_csv("./export_items.csv", encoding="utf-8_sig", columns=["name", "asin", "price", "star", "description"])
+    pass
 
 
 if __name__ == "__main__":
-    fire.Fire(run)
-    #run("https://www.amazon.co.jp/gp/bestsellers/automotive", 3)
+    pass
